fcf4053c6bafa18c4126ad9c2eb4017f05cb6adf
Delayless <Delayless@gmail.com>
2020-11-03 13:41:36 +0800

    modified the open_camera.sh

--- open_camera.sh
+++ open_camera.sh
@@ -1,4 +1,1 @@
-mpv --no-cache --untimed --demuxer-thread=no --video-sync=audio --vd-lavc-threads=1 --no-osc --no-input-default-bindings --no-config --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)
-
-# mpv --no-cache --untimed --no-demuxer-thread --video-sync=audio --vd-lavc-threads=1 --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)
-
+mpv --no-config --no-cache --untimed --demuxer-thread=no --video-sync=audio --vd-lavc-threads=1 --no-osc --no-input-default-bindings --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)
