9af64957ef4f7c64fb4a73a2705e6921336d8cae
Delayless <Delayless@gmail.com>
2020-11-16 01:56:01 +0800

    Set different wallpaper for different monitor and specify different picture paths. (random and distinguish between vertical and horizontal monitor).

--- wallpaper-autochange.sh
+++ wallpaper-autochange.sh
@@ -2,6 +2,7 @@
 
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=$(dirname $SCRIPT)
+sleep 1
 while true; do
     /bin/bash $SCRIPTPATH/wallpaper-change.sh
     sleep 30m
--- wallpaper-change.sh
+++ wallpaper-change.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-feh --bg-fill --recursive --randomize ~/Pictures/Wallpapers
+Monitor_1=$(find ~/Pictures/Wallpapers -iregex ".*[jp][pn][ge][g]?" | shuf -n1)
+Monitor_2=$(find ~/Pictures/Wallpapers -iregex ".*[jp][pn][ge][g]?" | shuf -n1)
+Monitor_3=$(find ~/Pictures/Vertical -iregex ".*[jp][pn][ge][g]?" | shuf -n1)
+feh --bg-fill $Monitor_1 $Monitor_2 $Monitor_3
+# feh --bg-fill --recursive --randomize ~/Pictures/Wallpapers
 # feh --bg-fill ~/Pictures/Wallpapers/wallpapers/ghibili/Howls\ Moving\ Castle/d7mkwt4.png
