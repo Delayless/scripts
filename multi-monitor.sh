ec61bca8885c5bce81ed7716e8567b7962a412a5
Delayless <Delayless@gmail.com>
2020-11-19 22:58:22 +0800

    multi-monitor.sh

--- multi-monitor.sh
+++ multi-monitor.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-xrandr --output HDMI-1 --right-of DP-1
-xrandr --output DP-1 --rotate left
-xrandr --output eDP-1 --right-of HDMI-1
-# xrandr --auto --output HDMI-1 --left-of eDP-1 --size 1920x1080
+# little screen
+xrandr --output DP-1 --auto --left-of HDMI-1 --rotate left
+# Dell Display
+xrandr --output HDMI-1 --auto --left-of eDP-1
+
