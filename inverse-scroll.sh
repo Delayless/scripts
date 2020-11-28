4331c1a46b1dfebe835ecb3ce9237eee05fb3d51
Delayless <Delayless@gmail.com>
2020-11-28 17:26:23 +0800

    refactor(inverse-scoll.sh)

--- inverse-scroll.sh
+++ inverse-scroll.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Get id of touchpad and the id of the field corresponding to
-# natural scrolling
-# Laptop Touchpad
-Touchpad_id=`xinput list | grep "Touchpad" | cut -d'=' -f2 | cut -d'[' -f1`
-# Apple Magic Trackpad
-Trackpad_id=`xinput list | grep "Trackpad" | cut -d'=' -f2 | cut -d'[' -f1`
-natural_scrolling_Touchpad_id=`xinput list-props $Touchpad_id \
-                      | grep "Natural Scrolling Enabled (" \
-                      | cut -d'(' -f2 | cut -d')' -f1`
-natural_scrolling_Trackpad_id=`xinput list-props $Trackpad_id \
-                      | grep "Natural Scrolling Enabled (" \
-                      | cut -d'(' -f2 | cut -d')' -f1`
+# Get id of touchpad and the id of the field corresponding to natural scrolling
+inverse_scroll() {
+	Touchpad_id=`xinput list | grep $1 | cut -d'=' -f2 | cut -d'[' -f1`
+	if [[ $Touchpad_id -ne 0 ]]; then
+		natural_scrolling_Touchpad_id=`xinput list-props $Touchpad_id \
+							  | grep "Natural Scrolling Enabled (" \
+							  | cut -d'(' -f2 | cut -d')' -f1`
+		# Set the property to true
+		xinput --set-prop $Touchpad_id $natural_scrolling_Touchpad_id 1
+	fi
+}
 
-# Set the property to true
-xinput --set-prop $Touchpad_id $natural_scrolling_Touchpad_id 1
-xinput --set-prop $Trackpad_id $natural_scrolling_Trackpad_id 1
+# Laptop Touchpad
+inverse_scroll "Touchpad"
+# Apple Trackpad
+inverse_scroll "Trackpad"
