d44431fd9fa2d53e8871b92291e3794a9709cc67
Delayless <Delayless@gmail.com>
2021-12-15 08:28:50 +0800

    perf: picom add some options

--- autostart.sh
+++ autostart.sh
@@ -2,7 +2,6 @@
 
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=$(dirname $SCRIPT)
-echo $SCRIPT
 # /bin/bash $SCRIPTPATH/feh.sh &
 /bin/bash $SCRIPTPATH/wallpaper-autochange.sh &
 /bin/bash $SCRIPTPATH/dwm-status.sh &
@@ -10,7 +9,15 @@ echo $SCRIPT
 nm-applet &
 blueman-tray &
 udiskie -s &
-# fcitx5 &
+# fcitx5 -d
 goldendict &
-picom -b
+# `xrandr --listproviders` to query the parameters
+xrandr --setprovideroffloadsink nouveau Intel
+# OR
+# xrandr --setprovideroffloadsink 1 0
+picom -b --unredir-if-possible --backend xr_glx_hybrid --vsync --use-damage --glx-no-stencil
+# without PRIME GPU offloading, xcompmgr is better maybe
+# xcompmgr -c &
+dunst &
 # xcompmgr -f -l 0.038 -O 0.040 -D 3 -F -C -n &
+# (fcitx5 > /dev/null 2>&1)&
